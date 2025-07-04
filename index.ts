bb3387b890beb376670aea2a6a05777d7b007847
Rubina Kumari <rubinakumariarora@gmail.com>
2025-07-04 19:30:29 +0100

    debug

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import db from "./db";
 import { Transaction } from "./types";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
